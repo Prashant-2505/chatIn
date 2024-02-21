c872a36e36adce6e1cba8cfae73a160e05a87145
Prashant-2505 <shramaprashant2505@gmail.com>
2024-02-21 15:02:57 +0530

    Data base Model created

---
+++ backend/models/messageModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const messageModel = mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    chat: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chat"
+    },
+    content: {
+        type: String,
+        trim: true
+    }
+}, { timestemps: true })
+
+const Message = mongoose.model("Message", messageModel)
+module.exports = Message
